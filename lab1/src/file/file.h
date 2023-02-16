98bbf87d1fb595747e2fd6f4a09297ff20409c58
Nanikon <311706@niuitmo.ru>
2023-02-16 22:16:21 +0300

    init struct

---
+++ lab1/src/file/file.h
@@ -0,0 +1,27 @@
+#ifndef LLP_FILE_H
+#define LLP_FILE_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+struct tree_header {
+    // Смещение первого узла дерева относительно начала файла
+    int64_t first_node;
+    // Смещение первой схемы узла относительно начала файла
+    int64_t first_schema;
+    // Смещение первого свободного блока относительно начала файла
+    int64_t first_free_block;
+    // у Фарида есть ещё size, у Маши - смещение конца файла
+};
+
+// tree_header лежит внутри, кажется что пользователь не должен иметь к нему доступ
+struct file_descriptor {
+    FILE* file;
+    struct tree_header header;
+};
+
+struct file_descriptor open_file(const char* filename);
+
+void close_file(struct file_descriptor fd);
+
+#endif 
---
+++ lab1/src/schema/schema.h
@@ -0,0 +1,25 @@
+#ifndef LLP_SCHEMA_H
+#define LLP_SCHEMA_H
+
+#include <vector>
+#include <string>
+
+enum type {
+    INT,
+    DOUBLE,
+    BOOL,
+    STRING
+};
+
+struct attribute_schema{ 
+    std::string name;
+    enum type type;
+};
+
+struct schema {
+    int count;
+    std::string name;
+    std::vector<struct attribute_schema> attributes;
+};
+
+#endif
